8741ea237a7f1f07bd9a81193c55cb35eab66b27
Chris Tijerina <crtijerina1988@gmail.com>
2021-10-13 16:00:52 -0500

    updating the fs

--- bookish-app/server/server.js
+++ bookish-app/server/server.js
@@ -1,44 +1,46 @@
 // bring this in from week 18, Activity 1 
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
 
-const express = require('express');
-const mongoose = require('mongoose');
 
-const app = express();
-const PORT = process.env.PORT || 3001;
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+// const app = express();
+// const PORT = process.env.PORT || 3001;
+// const MongoClient = require('mongodb').MongoClient;
+// const assert = require('assert');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-// Connection URL
-const url = 'mongodb+srv://$[username]:$[password]@$[hostlist]/$[database]?retryWrites=true';
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// // Connection URL
+// const url = 'mongodb+srv://$[username]:$[password]@$[hostlist]/$[database]?retryWrites=true';
 
-// Use connect method to connect to the Server
-// MongoClient.connect(url, function(err, client) {
-//   assert.equal(null, err);
-//   client.close();
+// // Use connect method to connect to the Server
+// // MongoClient.connect(url, function(err, client) {
+// //   assert.equal(null, err);
+// //   client.close();
+// // });
+// //const { User } = require('./models');
+
+
+
+
+//  mongoose.connect('mongodb+srv://Chris:12345!@#$%@cluster0.7brun.mongodb.net/myFirstDatabase?retryWrites=true&w=majorit', {
+// // //  useFindAndModify: false,
+//    useNewUrlParser: true,
+//    useUnifiedTopology: true
+//  });
+
+// // mongoose.set('useCreateIndex', true);
+// // mongoose.set('debug', true);
+
+
+// // app.get('/users', (req, res) => {
+// //   User.find({}).then(users => {
+// //     res.json(users);
+// //   });
+// // });
+
+// app.listen(PORT, () => {
+//   console.log(`App running on port ${PORT}!`);
 // });
-//const { User } = require('./models');
-
-
-
-
- mongoose.connect('mongodb+srv://Chris:12345!@#$%@cluster0.7brun.mongodb.net/myFirstDatabase?retryWrites=true&w=majorit', {
-// //  useFindAndModify: false,
-   useNewUrlParser: true,
-   useUnifiedTopology: true
- });
-
-// mongoose.set('useCreateIndex', true);
-// mongoose.set('debug', true);
-
-
-// app.get('/users', (req, res) => {
-//   User.find({}).then(users => {
-//     res.json(users);
-//   });
-// });
-
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-});
