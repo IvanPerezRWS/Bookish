84e5584439457ec383fd313661b4def59232a997
IvanPerezRWS <ivan.perez.rws@gmail.com>
2021-10-14 01:24:50 -0500

    Set up filestructure, routing, and controllers

---
+++ server/routes/posts.js
@@ -0,0 +1,13 @@
+
+import express from 'express';
+
+import{ getPosts, createPost } from '../controllers/posts.js'
+
+const router = express.Router();
+
+// http://localhost:3000/posts
+
+router.get('/', getPosts);
+router.post('/', createPost);
+
+export default router;
