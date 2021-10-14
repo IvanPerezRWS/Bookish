f61fecf67d1f29291a6677a01d72ef2320c25db4
IvanPerezRWS <ivan.perez.rws@gmail.com>
2021-10-14 01:55:13 -0500

    Added basic JSX structure to application

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,34 @@
 
 import React from 'react';
+// Check out documentation with this link:
+// https://www.npmjs.com/package/@material-ui/core
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+
+
+// Become familar with @material-ui and implement front end desing and styling
 
 const App = () => {
     return (
-        <div>
-            App
-        </div>
+        <Container maxWidth="lg">
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" align="center">Bookish</Typography>
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid containter justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
     )
 }
 
