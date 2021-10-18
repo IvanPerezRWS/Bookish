7770b4d32d5d1524e00571ba8760e10258c5a6f3
IvanPerezRWS <ivan.perez.rws@gmail.com>
2021-10-17 21:52:24 -0500

    Changed files for heroku deployment

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import postRoutes from './routes/posts.js';
 
 import dotenv from 'dotenv';
 
+
 const app = express();
 dotenv.config();
 
@@ -31,8 +32,6 @@ app.get('/', (req, res) => {
 
 // Connect app to mongodb on cloud atlas
 
-//const CONNECTION_URL = 'mongodb+srv://Chris:12345%21%40%23%24%25@cluster0.7brun.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -45,3 +44,15 @@ mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnified
 if (process.env === 'production') {
     app.use(express.static('client/build'));
 }
+
+
+// production build
+
+// accessing the path module
+const path = require("path");
+// import the client build folder to the server
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// ensure that routes defined with React router are workgin once the application has been deployed
+app.get("*", function (request, response) {
+    response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
